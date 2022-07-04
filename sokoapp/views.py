6e736d8334dae9ef0254d3f93b41b3cdf5454e2b
SayiaFelix <sayia.lucas@student.moringaschool.com>
2022-07-04 19:53:33 +0300

    setting views of about

--- sokoapp/views.py
+++ sokoapp/views.py
@@ -3,6 +3,8 @@ from django.shortcuts import render
 # Create your views here.
 def home(request):
     return render(request,"home.html")
+def about(request):
+    return render(request,"home.html")
 def women(request):
     return render(request,"women.html")
 def men(request):
