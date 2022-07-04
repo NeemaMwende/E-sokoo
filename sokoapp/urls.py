daa3fc72402f5b93f67b6c27ad0c9f5ce41d339f
Agnes-Kalunda <agnes.mbiti@student.moringaschool.com>
2022-07-04 14:13:45 +0300

    created route for woemn template

--- sokoapp/urls.py
+++ sokoapp/urls.py
@@ -7,7 +7,8 @@ from django.conf.urls.static import static
 urlpatterns=[
   
     url('', views.home, name='home'),
-    # url('search/', views.search, name='search'),
+    url('women/', views.women, name='women'),
+    
    
 ]
 
