a0287620acfd3042338621eef79ef21010e0b954
SayiaFelix <sayia.lucas@student.moringaschool.com>
2022-07-06 19:08:32 +0300

    creating Template

--- sokoapp/urls.py
+++ sokoapp/urls.py
@@ -1,18 +1,19 @@
+from django import views
 from django.urls import path
-from . import views
+from .views import *
 from django.conf import settings
 from django.conf.urls.static import static
 
 
 urlpatterns=[
  
-    path('', views.home, name='home'),
-    path('about/', views.about, name='about'),
-    path('addToCart/(\d+)', views.addToCart, name='addToCart'),
-    path('women/', views.women, name='women'),
-    path('men/', views.men, name='men'),
-    path('shop/', views.shop, name='shop'),
-    path('signup/', views.signup, name='signup'),
+    path('', home, name='home'),
+    path('about/', about, name='about'),
+    path('addToCart/<int:pro_id>', addToCart.as_view(), name='addToCart'),
+    path('women/', women, name='women'),
+    path('men/', men, name='men'),
+    path('shop/', shop, name='shop'),
+    path('signup/', signup, name='signup'),
     
    
 ]
--- sokoapp/views.py
+++ sokoapp/views.py
@@ -1,7 +1,8 @@
+from string import Template
 from django.shortcuts import render, redirect, get_object_or_404
 from django.http import HttpResponseRedirect
 from .forms import *
-from django.views.generic import ListView,DetailView
+from django.views.generic import TemplateView
 from django.utils import timezone
 from django.contrib.auth import login, authenticate
 from django.contrib.auth.decorators import login_required
@@ -25,9 +26,18 @@ def shop(request):
    clothes = Product.objects.all
    return render(request,"shop.html",{'clothes':clothes})
 
-def addToCart(request):
+class addToCart(TemplateView):
+    template_name = "cart.html"
+    # Get product by id
+
+
+    # get product
+
+    # check if cart exists
+
+    # check if product is already exists
  
-    return render(request,"cart.html")
+  
 
 def about(request):
     return render(request,"about.html")
