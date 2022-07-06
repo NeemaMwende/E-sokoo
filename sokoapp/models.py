04e0c6a2182521ce191b28b028311f9f14206479
SayiaFelix <sayia.lucas@student.moringaschool.com>
2022-07-06 18:29:39 +0300

    creating view

--- sokoapp/models.py
+++ sokoapp/models.py
@@ -1,32 +1,38 @@
 from django.db import models
 from django.contrib.auth.models import User
+from django.urls import reverse
 from tinymce.models import HTMLField
 
 # Create your models here.
-class Item(models.Model):
-    name = models.CharField(max_length=100)
+
+class Product(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
+    name = models.CharField(max_length=100)
     photo = models.ImageField(upload_to='product/')
     description = HTMLField()
     price = models.IntegerField()
-    quantity = models.IntegerField(default=1)
-   
+
+
     def __str__(self):
         return self.name
 
-class OrderItem(models.Model):
-    item = models.ForeignKey(Item, on_delete=models.CASCADE)
-    quantity = models.IntegerField(default=1)
-    
-    def __str__(self):
-        return self.name
 
-class Order(models.Model):
-    user = models.ForeignKey(User, on_delete=models.CASCADE)
-    item = models.ManyToManyField(OrderItem)
+ 
+class Cart(models.Model):
+    user = models.ForeignKey(User, on_delete=models.SET_NULL, null=True, blank=True)
+    total = models.PositiveIntegerField(default=0)
     date = models.DateTimeField(auto_now_add=True)
-    ordered=models.BooleanField(default=False)
-  
-   
+
     def __str__(self):
-        return self.user
+        return self.name
+
+class CartProduct(models.Model):
+    cart = models.ForeignKey(Cart, on_delete=models.CASCADE)
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
+    quantity = models.PositiveIntegerField()
+    subtotal = models.PositiveIntegerField()
+
+    def __str__(self):
+        return "cart: " + str(self.cart.id) + "CartProduct: " + str(self.id)
+        
+        
--- sokoapp/views.py
+++ sokoapp/views.py
@@ -1,15 +1,20 @@
 from django.shortcuts import render, redirect, get_object_or_404
 from django.http import HttpResponseRedirect
 from .forms import *
+from django.views.generic import ListView,DetailView
+from django.utils import timezone
 from django.contrib.auth import login, authenticate
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib import messages
 from .models import *
 # Create your views here.
+
+
+
 def home(request):
-    clothes = Item.objects.all
-    return render(request, 'home.html', {"clothes": clothes})
+    clothes = Product.objects.all
+    return render(request, 'home.html',{'clothes':clothes})
 
 def women(request):
     return render(request,"women.html")
@@ -17,23 +22,12 @@ def men(request):
     return render(request,"men.html")
     
 def shop(request):
-    clothes = Item.objects.all
-    return render(request,"shop.html",{"clothes": clothes})
+   clothes = Product.objects.all
+   return render(request,"shop.html",{'clothes':clothes})
 
-def addToCart(request,name):
-    item = get_object_or_404(Item,name=name)
-    orderItem = OrderItem.objects.create(item=item)
-    orderQs = Order.objects.filter(user=request.user, ordered=False)
-
-    if orderQs.exists():
-        order=orderQs[0]
-        #checking if order is already in cart
-        if order.items.filter(item__name=item.slug).exists():
-            orderItem.quantity += 1
-            orderItem.save()
-
-
-    return render(request,"cart.html")
+# def addToCart(request,slug):
+ 
+#     return render(request,"cart.html")
 
 def about(request):
     return render(request,"about.html")
