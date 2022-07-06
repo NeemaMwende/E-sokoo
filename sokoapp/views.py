d891464c76bf35fbf063d026f032a7830879c981
FloKots <florencekotohoyoh@gmail.com>
2022-07-06 15:09:30 +0300

    Define form in view function

--- sokoapp/views.py
+++ sokoapp/views.py
@@ -1,11 +1,28 @@
 from django.shortcuts import render
 
+from sokoapp.forms import NewsLetterForm
+
 # Create your views here.
 def home(request):
-    return render(request,"home.html")
+
+    if request.method == 'POST':
+        form = NewsLetterForm(request.POST)
+        if form.is_valid():
+            print('valid')
+        else:
+            form = NewsLetterForm()
+
+    return render(request,"home.html", {'letterForm': form})
+
+
 def women(request):
     return render(request,"women.html")
+
+
 def men(request):
     return render(request,"men.html")
+
+
 def shop(request):
     return render(request,"shop.html")
+
