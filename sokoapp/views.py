98f04bddc2f110e9315fdbcbf189653455b583db
FloKots <florencekotohoyoh@gmail.com>
2022-07-07 06:11:56 +0300

    Send welcome email

--- sokoapp/views.py
+++ sokoapp/views.py
@@ -3,6 +3,7 @@ from django.http import HttpResponse, Http404, HttpResponseRedirect
 
 from sokoapp.forms import NewsLetterForm
 from sokoapp.models import NewsLetterRecipients
+from .emails import send_welcome_email
 
 # Create your views here.
 def home(request):
@@ -12,14 +13,16 @@ def home(request):
         if form.is_valid():
             name = form.cleaned_data['name']
             email = form.cleaned_data['email']
+
             recipient = NewsLetterRecipients(name=name, email=email)
             recipient.save()
+            send_welcome_email(name,email)
             HttpResponseRedirect('home')
             print('valid')
     else:
         form = NewsLetterForm()
 
-    return render(request,"home.html", {'letterForm': form})
+    return render(request, "home.html", {'letterForm': form})
 
 
 def women(request):
